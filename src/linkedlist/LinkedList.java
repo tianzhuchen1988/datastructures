b37fc2e53ff15fbf05964104c53117d40bd7c8ce
chentz <3690560@qq.com>
2019-07-19 11:55:48 +0800

    linekedlist-add-01

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -23,4 +23,45 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
+
+    private Node head;
+    private int size;
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void addFirst(E e){
+        Node node = new Node(e);
+        node.next = head;
+        head = node;
+        size++;
+    }
+
+    public void add(int index, E e){
+        if(index < 0 || index > size){
+            throw new IllegalArgumentException("Index is invalid.");
+        }
+        if(index == 0){
+            addFirst(e);
+        }else{
+            Node prev = head;
+            for(int i = 0; i < index - 1; i++){
+                prev = prev.next;
+            }
+            Node newNode = new Node(e);
+            newNode.next = prev.next;
+            prev.next = newNode;
+            size++;
+        }
+    }
+
+    public void addLast(E e){
+        add(size, e);
+    }
+
 }
