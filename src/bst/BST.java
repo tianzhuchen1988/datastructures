bef124332241c40dfbb37f740d8b8fd4858aa2de
chentz <3690560@qq.com>
2019-07-23 11:29:01 +0800

    bst-add-01

--- src/bst/BST.java
+++ src/bst/BST.java
@@ -28,4 +28,30 @@ public class BST<E extends Comparable<E>> {
         return size == 0;
     }
 
+    public void add(E e){
+        if(root == null){
+            root = new Node(e);
+            size++;
+        }else{
+            add(root, e);
+        }
+    }
+
+    private void add(Node node, E e){
+        if(e.compareTo(node.e) == 0){
+            return;
+        }else if(e.compareTo(node.e) < 0 && node.left == null){
+            node.left = new Node(e);
+            size++;
+        }else if(e.compareTo(node.e) > 0 && node.right == null){
+            node.right = new Node(e);
+            size++;
+        }
+        if(e.compareTo(node.e) < 0){
+            add(node.left, e);
+        }else{
+            add(node.right, e);
+        }
+    }
+
 }
