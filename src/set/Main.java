00faa1ae1c41d4ee6f0c1b50ac03ba3985f3de85
chentz <3690560@qq.com>
2019-07-25 14:43:59 +0800

    linkedlistSet

--- src/set/Main.java
+++ src/set/Main.java
@@ -12,7 +12,7 @@ public class Main {
         if(FileOperation.readFile("C:\\Users\\Administrator\\Desktop\\tianmai3\\datastructures\\pride-and-prejudice.txt", words1)) {
             System.out.println("Total words: " + words1.size());
 
-            BSTSet<String> set1 = new BSTSet<>();
+            LinkedListSet<String> set1 = new LinkedListSet<>();
             for (String word : words1)
                 set1.add(word);
             System.out.println("Total different words: " + set1.getSize());
@@ -27,7 +27,7 @@ public class Main {
         if(FileOperation.readFile("C:\\Users\\Administrator\\Desktop\\tianmai3\\datastructures\\a-tale-of-two-cities.txt", words2)){
             System.out.println("Total words: " + words2.size());
 
-            BSTSet<String> set2 = new BSTSet<>();
+            LinkedListSet<String> set2 = new LinkedListSet<>();
             for(String word: words2)
                 set2.add(word);
             System.out.println("Total different words: " + set2.getSize());
